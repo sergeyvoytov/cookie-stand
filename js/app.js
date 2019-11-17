0ca672bdcedb541f2b101ac1e116045ba7926846
sergeyvoytov <sergvoytov@gmail.com>
2019-11-16 16:30:07 -0800

    fixed all the functions

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@
 // TODO: dynamically generate sales data
 
 'use strict';
+// Defining global variables
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var allShops = [];
+var shopContainer = document.getElementById('Shops');
+var article = addElement('article', shopContainer);
+var tableElem = addElement('table', article);
+var tableBody = addElement('tbody', tableElem);
+var tfoot = addElement('tfoot', tableElem);
+var thead = addElement('thead', tableElem);
+var footerRow = addElement('tr', tfoot);
+var form = document.getElementById('shop-form');
+
 //function to add element
 function addElement(tag, container, text) {
   var element = document.createElement(tag);
@@ -11,7 +21,7 @@ function addElement(tag, container, text) {
   element.textContent = text;
   return element;
 }
-//Creating an Object
+//Creating an Object Shop
 function Shop(shopName, minCustomers, maxCustomers, averageCookiePerCustomer) {
   this.shopName = shopName;
   this.minCustomers = minCustomers;
@@ -19,19 +29,19 @@ function Shop(shopName, minCustomers, maxCustomers, averageCookiePerCustomer) {
   this.averageCookiePerCustomer = averageCookiePerCustomer;
   this.hourlyResultsArray = [];
   this.totalSales = 0;
-  allShops.push[this];
+  getRandomCustomerSales(this);
+  allShops.push(this);
 }
-//method
+//Prototype method to render data for the table
 
 Shop.prototype.renderData = function () {
-
-  var tableTr = addElement('tr', tableElem);
+  var tableTr = addElement('tr', tableBody);
   addElement('td', tableTr, this.shopName);
   for (var i = 0; i < this.hourlyResultsArray.length; i++) {
     addElement('td', tableTr, this.hourlyResultsArray[i]);
   }
   addElement('td', tableTr, this.totalSales);
-}
+};
 
 // Random Customer sales
 function getRandomCustomerSales(shop) {
@@ -44,70 +54,25 @@ function getRandomCustomerSales(shop) {
 }
 // function to render Hours on header of the table
 
-function renderHours() {
-  var tableTr = addElement('tr', tableElem);
+function addHeaderRow() {
+  var tableTr = addElement('tr', thead);
   addElement('th', tableTr);
   for (var i = 0; i < hours.length; i++) {
     addElement('th', tableTr, hours[i]);
   }
   addElement('th', tableTr, 'Daily total');
 }
+// Added shops using constructor
+new Shop('Seattle', 23, 65, 6.3);
+new Shop('Tokyo', 3, 24, 1.2);
+new Shop('Dubai', 11, 38, 3.7);
+new Shop('Paris', 20, 38, 2.3);
+new Shop('Lima', 2, 16, 4.6);
 
-var seattle = new Shop('Seattle', 23, 65, 6.3);
-getRandomCustomerSales(seattle);
-
-var tokyo = new Shop('Tokyo', 3, 24, 1.2);
-getRandomCustomerSales(tokyo);
-
-var dubai = new Shop('Dubai', 11, 38, 3.7);
-getRandomCustomerSales(dubai);
-
-var paris = new Shop('Paris', 20, 38, 2.3);
-getRandomCustomerSales(paris);
-
-var lima = new Shop('Lima', 2, 16, 4.6);
-getRandomCustomerSales(lima);
-
-// var ShopContainer = document.getElementById('Shops');
-var shopContainer = document.getElementById('Shops');
-var article = addElement('article', shopContainer);
-var tableElem = addElement('table', article);
-
-renderHours();
-
-
-seattle.renderData();
-tokyo.renderData();
-dubai.renderData();
-paris.renderData();
-lima.renderData();
-
-//form handling
-var form = document.getElementById('shop-form');
-
-function submitHandler(event) {
-  event.preventDefault();
-  var shop = new Shop(event.target.shopName.value, parseInt(event.target.minCustomers.value), parseInt(event.target.maxCustomers.value), parseFloat(event.target.averageCookiePerCustomer.value));
-  //event.target.reset();
-  // shop.push(this);
-  shop.renderData(this);
-
-  // console.log('shopname',this.min);
-  // shop.getRandomCustomerSales();
-  // getRandomCustomerSales(shop);
-
-  // this.shop.push(allStores);
-}
-
-form.addEventListener('submit', submitHandler);
-
-
-///hourlyResultsArray
-
+// Adding footer row
 function addFooterRow() {
   //create tr for footer
   // create td elements 14 times + MEGA Total
-  var footerRow = addElement('tr', tableElem);
   addElement('td', footerRow, 'Totals');
   // var megaTotal = 0;
   for (var i = 0; i < hours.length; i++) {
@@ -115,27 +80,34 @@ function addFooterRow() {
     var megaTotal = 0;
     var td = document.createElement('td');
     footerRow.appendChild(td);
-
-    sumCities += seattle.hourlyResultsArray[i];
-    sumCities += tokyo.hourlyResultsArray[i];
-    sumCities += dubai.hourlyResultsArray[i];
-    sumCities += paris.hourlyResultsArray[i];
-    sumCities += lima.hourlyResultsArray[i];
+    for (var shop of allShops) {
+      sumCities += shop.hourlyResultsArray[i];
+    }
     td.textContent = sumCities;
   }
   var tdMega = document.createElement('td');
   footerRow.appendChild(tdMega);
-
-  megaTotal += seattle.totalSales;
-  megaTotal += tokyo.totalSales;
-  megaTotal += dubai.totalSales;
-  megaTotal += paris.totalSales;
-  megaTotal += lima.totalSales;
+  for (var shop2 of allShops) {
+    megaTotal += shop2.totalSales;
+  }
   tdMega.textContent = megaTotal;
-
-// for (var j=0, j < allShops.length; j++){
-//   megaTotal += shop.totalSales;
-// }
-
 }
+
+function submitHandler(event) {
+  event.preventDefault();
+  var shop = new Shop(event.target.shopName.value, parseInt(event.target.minCustomers.value), parseInt(event.target.maxCustomers.value), parseFloat(event.target.averageCookiePerCustomer.value));
+  // shop.push(this);
+  shop.renderData(this);
+  //handle updated footer row
+  footerRow.innerHTML = '';
+  addFooterRow();
+  event.target.reset();
+}
+form.addEventListener('submit', submitHandler);
+
+//Rending all rows
+for (var shop of allShops) {
+  shop.renderData();
+}
+addHeaderRow();
 addFooterRow();
